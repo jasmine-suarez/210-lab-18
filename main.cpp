8e8d130a0f27a39bd01c9d0ee23fc78f4a0faae1
Jasmine <jazzyysuarez@gmail.com>
2025-10-05 13:19:14 -0700

    update

--- main.cpp
+++ main.cpp
@@ -4,8 +4,22 @@
 #include <iostream>
 using namespace std;
 
+struct Node {
+    double rating;
+    string comment;
+    Node *next;
+};
+
 int main() {
-    
+   
+    int choice;
+
+    cout << "Which linked list method should we use?" << endl;
+    cout << "\t[1] New nodes are added at the head of the linked list" << endl;
+    cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
+    cout << "\tChoice: ";
+    cin >> choice;
+    cin.ignore();
 
     return 0;
 }
